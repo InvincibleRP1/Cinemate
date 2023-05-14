e1769398c525fe1b59a6db216fbac5e563d82e0f
Rahul <67526478+InvincibleRP1@users.noreply.github.com>
2023-05-14 21:39:17 +0530

    feat: added category area in home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,53 @@
-import { ContainerArea } from "../components/container/container"
-import { Navbar } from "../components/navbar/navbar"
+import { ContainerArea } from "../components/container/container";
+import { Navbar } from "../components/navbar/navbar";
+
+import "./pageStylings/home.css";
 
 export const HomePage = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <ContainerArea></ContainerArea>
 
-    return (
-        <div>
-            <Navbar></Navbar>
-            <ContainerArea></ContainerArea>
-        </div>
-    )
-}
+      <div className="category-section">
+        <ul className="category-list">
+          <li className="category-item">
+            <div className="category-heading">Category</div>
+            <img
+              src="https://ik.imagekit.io/qsdtqu5hp/cinemate-images/holly-logo.jpg?updatedAt=1684079038065"
+              alt="category"
+              className="category-image"
+            />
+            <p className="category-text">
+            Get the latest blockbusters and cult classics from Hollywood.
+            </p>
+          </li>
+
+          <li className="category-item">
+            <div className="category-heading">Category</div>
+            <img
+              src="https://ik.imagekit.io/qsdtqu5hp/cinemate-images/holly-logo.jpg?updatedAt=1684079038065"
+              alt="category"
+              className="category-image"
+            />
+            <p className="category-text">
+            Get the latest blockbusters and cult classics from Hollywood.
+            </p>
+          </li>
+
+          <li className="category-item">
+            <div className="category-heading">Category</div>
+            <img
+              src="https://ik.imagekit.io/qsdtqu5hp/cinemate-images/holly-logo.jpg?updatedAt=1684079038065"
+              alt="category"
+              className="category-image"
+            />
+            <p className="category-text">
+            Get the latest blockbusters and cult classics from Hollywood.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
